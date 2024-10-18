76705a419ccf9a62b1214092e78d8dcc6580287c
Luka-Pereira <luka.pereira.pro@gmail.com>
2024-10-18 10:33:02 +0200

    Add files via upload

---
+++ Convertisseur_de_degre/MaFenetre.h
@@ -0,0 +1,35 @@
+#include <QApplication>
+#if QT_VERSION >= 0x050000
+#include <QtWidgets> /* tous les widgets de Qt5 */
+#else
+#include <QtGui> /* tous les widgets de Qt4 */
+#endif
+#define CELCIUS_FARENHEIT 0
+#define FARENHEIT_CELCIUS 1
+class MaFenetre : public QDialog
+{
+Q_OBJECT
+// Membre(s) public(s)
+public:
+MaFenetre( QWidget *parent = 0 );
+// Membre(s) privé(s)
+private:
+// Les widgets
+QLineEdit *valeur;
+QLabel *resultat;
+QLabel *unite;
+QComboBox *choixConversion;
+QPushButton *bConvertir;
+QPushButton *bQuitter;
+QDoubleValidator *doubleValidator;
+// …………
+void afficherUnite();
+// Mécanisme(s) Qt
+signals:
+void actualiser();
+
+private slots:
+void convertir();
+void permuter();
+
+};
